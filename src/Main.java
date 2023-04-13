f48b7ea3bdbf50c32ee71711c563cff1f6aa070e
Maksim Lanin <maksimlanin@mail.ru>
2023-04-13 19:35:52 +0300

    Seminar 5 LaninMS

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import Seminar3.S3Task3;
 import Seminar4.S4Task1;
 import Seminar4.S4Task2;
 import Seminar4.S4Task3;
+import Seminar5.S5Task1;
+import Seminar5.S5Task2;
+import Seminar5.S5Task3;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,20 +26,39 @@ public class Main {
 //        s3Task3.Value();
 //
 //        System.out.println("\n--------------------");
-//      Seminar 4 Task 1
-        S4Task1 s4Task1 = new S4Task1();
-        s4Task1.RandomFilling();
-        s4Task1.RevertList();
+////      Seminar 4 Task 1
+//        S4Task1 s4Task1 = new S4Task1();
+//        s4Task1.RandomFilling();
+//        s4Task1.RevertList();
+//
+//        System.out.println("\n--------------------");
+//
+//        S4Task2 s4Task2 = new S4Task2();
+//        s4Task2.Metod();
+//
+//        System.out.println("\n--------------------");
+//
+//        S4Task3 s4Task3 = new S4Task3();
+//        s4Task3.Calculator();
+
+//        Seminar 5
 
         System.out.println("\n--------------------");
 
-        S4Task2 s4Task2 = new S4Task2();
-        s4Task2.Metod();
+        S5Task1 s5Task1 = new S5Task1();
+        s5Task1.TelephoneBook();
 
         System.out.println("\n--------------------");
 
-        S4Task3 s4Task3 = new S4Task3();
-        s4Task3.Calculator();
+        S5Task2 s5Task2 = new S5Task2();
+        s5Task2.StaffList();
+
+        System.out.println("\n--------------------");
+
+        S5Task3 s5Task3 = new S5Task3();
+        s5Task3.Chess();
+
+
 
 
 
